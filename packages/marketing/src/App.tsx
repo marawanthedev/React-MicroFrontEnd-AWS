efabcaaabf100d652f8f8b19b3c88338d19b88b2
marawanthedev <tmarawan11@gmail.com>
2022-11-03 14:41:04 +0800

    changing route configs at mareketing app

--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core"
 import Album from "./components/Landing/Landing";
 import Pricing from "./components/Pricing/Pricing";
@@ -13,14 +13,12 @@ export default function App(): JSX.Element {
   console.log("Marketing micro-front-end");
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <StylesProvider generateClassName={generateClassName}>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route exact path="/" component={Album} />
-        </StylesProvider>
-      </Switch>
-    </BrowserRouter>
+    <Router>
+      <StylesProvider generateClassName={generateClassName}>
+        <Route exact path="/pricing" component={Pricing} />
+        <Route exact path="/" component={Album} />
+      </StylesProvider>
+    </Router>
 
   );
 }
