0dd4a83b47577b8245a25c531be472eb08f2a25c
marawanthedev <tmarawan11@gmail.com>
2022-11-03 14:50:02 +0800

    Another attempt to fix production prefix

--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core"
 import Album from "./components/Landing/Landing";
 import Pricing from "./components/Pricing/Pricing";
 
 // adding different prefix so that each project has a different prefix and avoid colliding with other projects
 const generateClassName = createGenerateClassName({
-  productionPrefix: "marketing"
-})
+  productionPrefix: 'ma',
+});
 
 export default function App(): JSX.Element {
   console.log("Marketing micro-front-end");
 
   return (
-    <>
-      <Router>
-        <StylesProvider generateClassName={createGenerateClassName({
-          productionPrefix: "marketing"
-        })}>
-          <Route exact path="/pricing" component={Pricing} />
-          <Route exact path="/" component={Album} />
+    <div>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <Switch>
+            <Route exact path='/pricing' component={Pricing} />
+            <Route path='/' component={Album} />
+          </Switch>
         </StylesProvider>
-      </Router>
-    </>
+      </BrowserRouter>
+    </div>
 
 
   );
