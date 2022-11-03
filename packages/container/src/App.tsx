bf23133f9bdec3af507a6328218ad3eae436b287
marawanthedev <tmarawan11@gmail.com>
2022-11-03 14:04:12 +0800

    Adding context based production styling prefix to avoid css naming collisions

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./components/Header/Header";
 import MarketingApp from "./components/Marketing/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider,createGenerateClassName } from "@material-ui/core";
+
+const generateClassName=createGenerateClassName({productionPrefix:"host"})
+
 export default function App() {
   console.log("host of micro-front-end");
 
   return (
     <BrowserRouter>
-    <>
+    <StylesProvider generateClassName={generateClassName}>
       <Header signedIn={true} onSignOut={()=>{}}/>
       <MarketingApp />
-    </>
+    </StylesProvider>
     </BrowserRouter>
   );
 }
--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider,createGenerateClassName } from "@material-ui/core";
 import Album from "./components/Landing/Landing";
 import Pricing from "./components/Pricing/Pricing";
+
+// adding different prefix so that each project has a different prefix and avoid colliding with other projects
+const generateClassName=createGenerateClassName({
+  productionPrefix:"ma"
+})
 
 export default function App(): JSX.Element {
   console.log("Marketing micro-front-end");
 
   return (
     <>
-      <StylesProvider>
+      <StylesProvider  generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
