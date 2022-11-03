70f3d2849ee597748899ebcdf6ef92ceec87abb1
marawanthedev <tmarawan11@gmail.com>
2022-11-03 14:38:03 +0800

    Attempt to fix marketing production css prefix issue

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import Header from "./components/Header/Header";
 import MarketingApp from "./components/Marketing/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
-import {StylesProvider,createGenerateClassName} from "@material-ui/core/styles"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
 
 
-const generateClassName=createGenerateClassName({productionPrefix:"host"})
+const generateClassName = createGenerateClassName({ productionPrefix: "host" })
 
-export default function App():JSX.Element{
+export default function App(): JSX.Element {
   console.log("host of micro-front-end");
 
   return (
     <BrowserRouter>
-    <StylesProvider generateClassName={generateClassName}>
-      <Header signedIn={true} onSignOut={()=>{}}/>
-      <MarketingApp />
-    </StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <Header signedIn={true} onSignOut={() => { }} />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 }
--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import {StylesProvider,createGenerateClassName} from "@material-ui/core/styles"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core"
 import Album from "./components/Landing/Landing";
 import Pricing from "./components/Pricing/Pricing";
 
 // adding different prefix so that each project has a different prefix and avoid colliding with other projects
-const generateClassName=createGenerateClassName({
-  productionPrefix:"marketing"
+const generateClassName = createGenerateClassName({
+  productionPrefix: "marketing"
 })
 
 export default function App(): JSX.Element {
   console.log("Marketing micro-front-end");
 
   return (
-    <StylesProvider  generateClassName={generateClassName}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route exact path="/" component={Album} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
+    <BrowserRouter>
+      <Switch>
+        <StylesProvider generateClassName={generateClassName}>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/" component={Album} />
+        </StylesProvider>
+      </Switch>
+    </BrowserRouter>
 
   );
 }
