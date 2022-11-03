8bc54e0e26b76cdeb0490d3cc55a7f8bbc90270d
marawanthedev <tmarawan11@gmail.com>
2022-11-03 14:52:47 +0800

    Another attempt to fix production prefix

--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -5,15 +5,14 @@ import Album from "./components/Landing/Landing";
 import Pricing from "./components/Pricing/Pricing";
 
 // adding different prefix so that each project has a different prefix and avoid colliding with other projects
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
-});
+const generateClassName = createGenerateClassName({ productionPrefix: "marketing" })
+
 
 export default function App(): JSX.Element {
   console.log("Marketing micro-front-end");
 
   return (
-    <div>
+    <>
       <BrowserRouter>
         <StylesProvider generateClassName={generateClassName}>
           <Switch>
@@ -22,7 +21,7 @@ export default function App(): JSX.Element {
           </Switch>
         </StylesProvider>
       </BrowserRouter>
-    </div>
+    </>
 
 
   );
